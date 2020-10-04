04ac7bf78977d90a77e94af0200a8ff8c8ea237b
Yoshifumi-T <2g.4423@gmail.com>
2020-10-04 15:43:57 +0900

    first commit

---
+++ src/script/main.js
@@ -0,0 +1,9 @@
+import { Menu } from './menu';
+
+const init = () => {
+    const menu = new Menu();
+}
+
+document.addEventListener('load', init(), false);
+
+
---
+++ src/script/menu.js
@@ -0,0 +1,11 @@
+class Menu {
+    constructor() {
+        this.isActive = false;
+        document.addEventListener('click', onClick(), false);
+    }
+
+    onClick() {
+    }
+}
+
+export default Menu;
